55b949991f0b7eb5e56fedbd46d708407f62f813
wayne <muscle2765@sina.com>
2018-08-20 15:34:08 +0800

    format

--- src/main/java/nettyDemo/ClientStart.java
+++ src/main/java/nettyDemo/ClientStart.java
@@ -3,18 +3,18 @@ package nettyDemo;
 import java.util.Scanner;
 
 public class ClientStart {
-	public static void main(String[] args){  
+	
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-        Client bootstrap = new Client(8080, "127.0.0.1");  
-        
-        String infoString = "";
-        while (true){
-        	infoString = input.nextLine();
-        	RequestInfo req = new RequestInfo();  
-            req.setType((byte) 1);  
-            req.setInfo(infoString);
-            bootstrap.sendMessage(req);  
-           
-        }  
-    }  
+		Client bootstrap = new Client(8080, "127.0.0.1");
+
+		String infoString = "";
+		while (true) {
+			infoString = input.nextLine();
+			RequestInfo req = new RequestInfo();
+			req.setType((byte) 1);
+			req.setInfo(infoString);
+			bootstrap.sendMessage(req);
+		}
+	}
 }
--- src/main/java/nettyDemo/ServerStart.java
+++ src/main/java/nettyDemo/ServerStart.java
@@ -1,5 +1,6 @@
 package nettyDemo;
 
+//https://blog.csdn.net/qq_39658819/article/details/79389528
 public class ServerStart {
 	
 	public static void main(String[] args){
